b45afb92f336c3f886b95544bcaba2e9fd66fa51
Dhruv Vohra <dhruv.v405@gmail.com>
2019-06-28 13:39:07 +0530

    Constructor overloading

--- constOverloading.java
+++ constOverloading.java
@@ -5,12 +5,12 @@ class student {
     String name;
     double gpa;
 
-    student(int i, String n) {
+    student(int i, String n) {      //constructor-1 (with 2 param)
         id = i;
         name = n;
     }
 
-    student(int i, String n, double g) {
+    student(int i, String n, double g) {    // constructor-2 (with 3 param)
         id = i;
         name = n;
         gpa = g;
@@ -29,3 +29,4 @@ public class constOverloading {
         s2.display();
     }
 }
+//s1 gpa will be 0.0 as it is not assinged
