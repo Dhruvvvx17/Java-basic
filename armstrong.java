3e77acca3a07358167354c70beeb5701063cb6e3
Dhruv Vohra <dhruv.v405@gmail.com>
2019-06-28 13:25:48 +0530

    Armstrong number

--- armstrong.java
+++ armstrong.java
@@ -1,3 +1,6 @@
+//java program to find if a number is an armstrong number or not.
+//armstrong number - when the sum of cube of every digit = the number itself 
+
 public class armstrong{
     public static void main(String args[]) {
         int num = 153;
