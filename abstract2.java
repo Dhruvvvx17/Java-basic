6b34b2784bda4d0faedefc582232f07e324d1ca7
Dhruv Vohra <dhruv.v405@gmail.com>
2019-06-28 13:15:14 +0530

    abstraction + inheritance

--- abstract2.java
+++ abstract2.java
@@ -1,9 +1,13 @@
+//abstract class - cannot be instantiated, has to be extended
+
 abstract class bike{
+    //default constructor
     bike(){
         System.out.println("Bike manufactured");
     }
-
+    //abstract method
     abstract void run();
+    //non abstract method
     void gear(){
         System.out.println("Gear changed");
     }
@@ -13,7 +17,6 @@ class dio extends bike{
     void run(){
         System.out.println("Dio running safely");
     }
-
     //compiler creates a default constructor "dio()" whose first line is "super();" which invokes the default constructor of the parent call.
 }
 
